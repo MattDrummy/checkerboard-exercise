d62c28545564bb14bcd09e448586f08c6291fedd
Matthew Drumheller <MattDrummy@gmail.com>
2017-06-12 15:35:46 -0600

    flash

--- randomColors.js
+++ randomColors.js
@@ -1,4 +1,5 @@
 // Your JS goes here
+
 var body = document.getElementsByTagName('body')[0];
 body.style.boxSizing = 'borderBox';
 body.style.margin = '0';
@@ -19,3 +20,18 @@ for (var a = 0; a < 7; a++) {
   }
   body.appendChild(row);
 }
+
+function colorChangeTimer() {
+  var squareArray = document.getElementsByTagName('div');
+  console.log("fire1");
+  console.log(squareArray);
+  for (var i = 0; i < squareArray.length; i++) {
+    console.log("fire2");
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    squareArray[i].style.background = 'rgb(' + r + ',' + g + ',' + b + ')';
+  }
+}
+
+setInterval(colorChangeTimer, 500)
