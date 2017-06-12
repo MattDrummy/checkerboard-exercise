115202a162565b5a4713cd8d8f6a61aa2167eb7d
Matthew Drumheller <MattDrummy@gmail.com>
2017-06-12 12:55:47 -0600

    update

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // Your JS goes here
 var body = document.getElementsByTagName('body')[0];
 
-for (var a = 0; a < 9; a++) {
+for (var a = 0; a < 7; a++) {
   var row = document.createElement('section');
   row.style.height = '100px';
-  row.style.width = '100%';
   row.style.display = 'flex';
   for (var i = 0; i < 9; i++) {
     var square = document.createElement('div');
-    square.style.width = '100px';
+    square.style.width = '11.1%';
+    square.style.paddingBottom = '11.1%'
     if (i % 2 === 0 && a % 2 === 0) {
       square.style.background = 'black';
     } else if (i % 2 !== 0 && a % 2 === 0) {
