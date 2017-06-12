6dac2817a8e383e8e389add2c2b8b2207e435287
Matthew Drumheller <MattDrummy@gmail.com>
2017-06-12 12:53:31 -0600

    first board done

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // Your JS goes here
+var body = document.getElementsByTagName('body')[0];
+
+for (var a = 0; a < 9; a++) {
+  var row = document.createElement('section');
+  row.style.height = '100px';
+  row.style.width = '100%';
+  row.style.display = 'flex';
+  for (var i = 0; i < 9; i++) {
+    var square = document.createElement('div');
+    square.style.width = '100px';
+    if (i % 2 === 0 && a % 2 === 0) {
+      square.style.background = 'black';
+    } else if (i % 2 !== 0 && a % 2 === 0) {
+      square.style.background = 'red';
+    } else if (i % 2 === 0 && a % 2 !== 0) {
+      square.style.background = 'red';
+    } else {
+      square.style.background = 'black';
+    }
+    row.appendChild(square);
+  }
+  body.appendChild(row);
+}
